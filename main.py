706113bd234a0d1cf71cb5296b85cc61fc837f85
Alok Desai <alokdesai2004@gmail.com>
2024-11-22 18:09:12 +0530

    chore: add peerlist

--- main.py
+++ main.py
@@ -12,29 +12,29 @@ def main():
 
     node = Node(blockchain,host,port)
 
-    while True:
-        peer_ip = 'localhost'
-        peer_port = int(input("Enter peer port or -1 to quit: "))
+    with open('peerlist.bin', 'rb') as f:
+        peer_ports = f.read().splitlines()
 
-        if (peer_port == -1):
-            break
-
-        if (peer_ip, peer_port) not in node.peers:
-            node.peers.append((peer_ip, peer_port))
-        else:
-            print("Peer already exists.")
+    for peer_port in peer_ports:
+        peer_port = int(peer_port)
+        if peer_port != port:
+            node.peers.append((host, peer_port))
 
     threading.Thread(target=node.listen, daemon=True).start()
 
     print(f"Node started at {host}:{port}, waiting for input")
 
+    mined_block=None
     while True:
         action = input("\nOptions:\n - 't': Add a transaction\n - 'm': Mine a new block\n"
                        " - 'v': View the blockchain\n - 'b': Broadcast last mined block\n"
                        " - 'l': Show current latency stats\n - 'q': Quit\nChoose an option: ")
         
         if action == 't':
-            transaction = input("Enter transaction data: ")
+            _from = input("Enter sender address: ")
+            _to = input("Enter receiver address: ")
+            _amt = float(input("Enter amount: "))
+            transaction=f"{_from}->{_to}::{_amt}"
             blockchain.add_transaction(transaction)
             node.broadcast({"data": transaction,"type":'transaction'}, "transaction")
             print("Transaction added and broadcasted.")
@@ -43,8 +43,8 @@ def main():
             blockchain.mine_pending_transactions()
             new_block = blockchain.get_latest_block()
             message = {"type": "block", "data": new_block.__dict__}
-            node.broadcast(message, "block")
-            print(f"Block mined and broadcasted: {new_block.hash}")
+            print(f"Block mined: {new_block.hash}")
+            print(f"Press 'b' to broadcast the block to peers.")
 
         elif action == 'v':
             for block in blockchain.chain:
@@ -53,7 +53,8 @@ def main():
         elif action == 'b':
             if len(blockchain.chain) > 1:
                 last_block = blockchain.get_latest_block()
-                node.broadcast(last_block.__dict__, "block")
+                message = {"type": "block", "data": last_block.__dict__}
+                node.broadcast(message, "block")
                 print(f"Last block broadcasted: {last_block.hash}")
             else:
                 print("No blocks to broadcast.")
